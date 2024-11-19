2fecd2f21e030ac06d0d45b8fac8194040ea0a1b
KarinaBaida2003 <baidakarina2003@gmail.com>
2024-11-19 20:15:47 +0200

    Delete Task_2.py

--- Task_2.py
+++
@@ -1,8 +0,0 @@
-def factorial_recursive(n):
-    if n == 0 or n == 1:
-        return 1
-    return n * factorial_recursive(n - 1)
-
-# Приклад використання:
-n = int(input("Введіть число n: "))
-print(f"Факторіал {n}! = {factorial_recursive(n)}")
